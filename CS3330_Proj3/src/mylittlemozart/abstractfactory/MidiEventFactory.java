401f95e468deda31377cce16f4742321904973cd
DaneBishop <MelanieBishop@localhost>
2025-03-31 17:59:18 -0500

    Implemented the already given MidiEventData class

---
+++ CS3330_Proj3/src/mylittlemozart/abstractfactory/MidiEventFactory.java
@@ -0,0 +1,5 @@
+package mylittlemozart.abstractfactory;
+
+public interface MidiEventFactory {
+
+}
---
+++ CS3330_Proj3/src/mylittlemozart/midieventdata/MidiEventData.java
@@ -0,0 +1,92 @@
+package mylittlemozart.midieventdata;
+
+public class MidiEventData {
+
+	private int startEndTick, velocity, note, channel, noteOnOff;
+	private int instrument;
+	
+	public MidiEventData(int startEndTick, int velocity, int note, int channel, int instrument, int noteOnOff) {
+		this.startEndTick = startEndTick;
+		this.velocity = velocity;
+		this.note = note;
+		this.channel = channel;
+		this.instrument = instrument;
+		this.noteOnOff = noteOnOff;
+	}
+	
+	
+	
+	// GETTERS AND SETTERSs
+
+	
+	// StartEndTick
+	public int getStartEndTick() {
+		return startEndTick;
+	}
+
+	public void setStartEndTick(int startEndTick) {
+		this.startEndTick = startEndTick;
+	}
+
+	
+	
+	// Velocity
+	public int getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(int velocity) {
+		this.velocity = velocity;
+	}
+
+	
+	
+	// Note
+	public int getNote() {
+		return note;
+	}
+
+	public void setNote(int note) {
+		this.note = note;
+	}
+
+	
+	
+	// Channel
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+
+	
+	
+	
+	// NoteOnOff
+	public int getNoteOnOff() {
+		return noteOnOff;
+	}
+
+	public void setNoteOnOff(int noteOnOff) {
+		this.noteOnOff = noteOnOff;
+	}
+
+	
+	
+	
+	// Instrument
+	public int getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(int instrument) {
+		this.instrument = instrument;
+	}
+	
+	
+	
+	
+	
+}
