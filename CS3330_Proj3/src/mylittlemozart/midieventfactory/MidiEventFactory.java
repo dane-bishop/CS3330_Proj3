4e2508fbef9a5f9a9d991ef1fe670831bd03858c
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-04-01 16:54:58 -0500

    Created the abstract factories, added unimplemented methods.

--- CS3330_Proj3/src/mylittlemozart/midieventfactory/MidiEventFactory.java
+++ CS3330_Proj3/src/mylittlemozart/midieventfactory/MidiEventFactory.java
@@ -6,6 +6,7 @@ import mylittlemozart.midieventdata.MidiEventData;
 
 public interface MidiEventFactory {
 	
+	// forces subclasses to have these two functions that create noteon, noteoff
 	MidiEventData createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
 	MidiEventData createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 
---
+++ CS3330_Proj3/src/mylittlemozart/midieventfactoryabstract/StraccatoMidiEventFactoryAbstract.java
@@ -0,0 +1,13 @@
+package mylittlemozart.midieventfactoryabstract;
+
+import mylittlemozart.midieventfactory.MidiEventFactory;
+
+public class StraccatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract{
+
+	@Override
+	public MidiEventFactory createFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
