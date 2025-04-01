eb976f59e8d700366daaa4bfe547e48ff63568a7
DaneBishop <MelanieBishop@localhost>
2025-04-01 17:36:30 -0500

    Implemented the applyInstrument methods for all instrument strategies and also created the InstrumentImplementation class in order to actually set the Midi instrument based on the Instrument strategy

--- CS3330_Proj3/src/mylittlemozart/instrumentstrategy/ElectricBassGuitarStrategy.java
+++ CS3330_Proj3/src/mylittlemozart/instrumentstrategy/ElectricBassGuitarStrategy.java
@@ -1,5 +1,22 @@
 package mylittlemozart.instrumentstrategy;
 
+
+
+import javax.sound.midi.*;
+
 public class ElectricBassGuitarStrategy implements InstrumentStrategy{
+	
+	
+	/**
+	 * Implementing the applyInstrument method by calling the static method setInstrument
+	 * from the InstrumentImplementation class
+	 */
+	@Override
+	public void applyInstrument(Track track, int channel) {
+		InstrumentImplementation.setInstrument(track, channel, 33);
+	}
+	
+	
+	
 
 }
