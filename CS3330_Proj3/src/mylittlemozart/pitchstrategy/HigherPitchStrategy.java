1ee0189232659d3e814c038e9f8641f990b57549
DaneBishop <MelanieBishop@localhost>
2025-04-01 17:06:17 -0500

    Added "Override" in the appropriate places

--- CS3330_Proj3/src/mylittlemozart/pitchstrategy/HigherPitchStrategy.java
+++ CS3330_Proj3/src/mylittlemozart/pitchstrategy/HigherPitchStrategy.java
@@ -6,6 +6,7 @@ public class HigherPitchStrategy implements PitchStrategy{
 	 * Implementation of the modifyPitch method
 	 * Used to raise the pitch of a note
 	 */
+	@Override
 	public int modifyPitch(int note) {
 		note += 2;
 		return note;
--- CS3330_Proj3/src/mylittlemozart/pitchstrategy/LowerPitchStrategy.java
+++ CS3330_Proj3/src/mylittlemozart/pitchstrategy/LowerPitchStrategy.java
@@ -7,6 +7,7 @@ public class LowerPitchStrategy implements PitchStrategy{
 	 * Implementation of the modifyPitch method
 	 * Used to drop the pitch of a note
 	 */
+	@Override
 	public int modifyPitch(int note) {
 		note -= 2;
 		return note;
