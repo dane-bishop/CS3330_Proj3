95087ce0d1a95717efa920b60af694a12008b655
DaneBishop <MelanieBishop@localhost>
2025-04-01 16:59:20 -0500

    Created applyInstrument method in the InstrumentStrategy interface

--- CS3330_Proj3/src/mylittlemozart/instrumentstrategy/InstrumentStrategy.java
+++ CS3330_Proj3/src/mylittlemozart/instrumentstrategy/InstrumentStrategy.java
@@ -1,6 +1,15 @@
 package mylittlemozart.instrumentstrategy;
 
+import javax.sound.midi.*;
+
 public interface InstrumentStrategy {
 	// void applyInstrument(Track track, int channel)
+	
+	/**
+	 * Created method to be implemented in all instrument strategy classes
+	 * @param track
+	 * @param channel
+	 */
+	public void applyInstrument(Track track, int channel);
 
 }
