1e42e245cb754dd2d92c71f157879f611cf21214
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-04-01 16:24:21 -0500

    Got most of the parser working. Testing in main

--- CS3330_Proj3/src/mylittlemozart/Main.java
+++ CS3330_Proj3/src/mylittlemozart/Main.java
@@ -1,10 +1,33 @@
 package mylittlemozart;
 
+import java.util.List;
+
+import mylittlemozart.csvparser.CSVParser;
+import mylittlemozart.midieventdata.MidiEventData;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// this is the main method
 		// spencer
+		
+		
+        CSVParser parser = new CSVParser();
+        List<MidiEventData> midiEvents = parser.parseCsv("CS3330_Proj3\\mystery_song.csv");
+        //"C:\Users\Spencer Steinmeyer\git\CS3330_Proj3\CS3330_Proj3\mystery_song.csv"
+
+        // Just for demonstration, print out the parsed objects
+        
+        for (MidiEventData event : midiEvents) {
+            System.out.println("StartEndTick: " + event.getStartEndTick() +
+                               ", Velocity: " + event.getVelocity() +
+                               ", Note: " + event.getNote() +
+                               ", Channel: " + event.getChannel() +
+                               ", Instrument: " + event.getInstrument() +
+                               ", NoteOnOff: " + event.getNoteOnOff());
+        
+	}
+	
 	}
 
 }
--- CS3330_Proj3/src/mylittlemozart/csvparser/CSVParser.java
+++ CS3330_Proj3/src/mylittlemozart/csvparser/CSVParser.java
@@ -1,5 +1,62 @@
 package mylittlemozart.csvparser;
 
-public class CSVParser {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import mylittlemozart.midieventdata.MidiEventData;
+
+public class CSVParser {
+    
+	/**
+	 * takes a file path, initializes an empty arrayList,
+	 * opens a BufferedReader, splits data into a list by making each element before a comma into an element in the list
+	 * checks if data length is 6 to avoid errors, and then assigns all elements of that list to a MidiEventData object
+	 * after turning note_on_c into a 1, note_off_c into a 0
+	 * @param filePath
+	 * @return
+	 */
+    public List<MidiEventData> parseCsv(String filePath) {
+        List<MidiEventData> events = new ArrayList<>();
+        String line;
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 6) {
+                    int startEndTick = Integer.parseInt(data[0]);
+                    int noteOnOff = 0;
+                    // if the string is "Note_off_c" sets a 0, if the note is on, sets a 1
+//                    System.out.println(data[1]);
+                    if (data[1] == " Note_off_c") {
+                    	noteOnOff = 0;
+                    }
+                    else if(data[1] == " Note_on_c") {
+                    	noteOnOff = 1;
+                    }
+                    else {
+                    	noteOnOff = 2;
+                    }
+                    int channel = Integer.parseInt(data[2]);
+                    int note = Integer.parseInt(data[2]);
+                    int velocity = Integer.parseInt(data[3]);
+                    int instrument = Integer.parseInt(data[4]);
+                    
+                    
+                    
+                    MidiEventData event = new MidiEventData(startEndTick, velocity, note, channel, instrument, noteOnOff);
+                    events.add(event);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+        return events;
+    }
+
+    
+    
 }
+
